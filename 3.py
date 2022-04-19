ae64a156075230824b3e807f9887ce0ca115bbe0
RostislavGerasenko <91475179+RostislavGerasenko@users.noreply.github.com>
2022-04-19 04:20:03 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,6 +4,6 @@ s = input("Введите строку: ")
 number = 1
 
 while number:
-   s, number = re.subn(r'(^| )([а-я])(\S*)(\2)', r'\1\2\3.', s)
+   s, number = re.subn(r'(^| )([А-Я])([А-Я]*)(\2)', r'\1\2\3.', s)
 
 print(f"Новая строка: {s}")
